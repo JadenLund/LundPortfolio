1609a81322115e2c70c7ac494fbebf7524c5c0d3
JadenLund <lundjaden@gmail.com>
2023-05-22 12:09:30 -0500

    test

---
+++ src/components/BottomText.jsx
@@ -0,0 +1,8 @@
+export default function Bottomtext() {
+  return (
+    <p>
+      This application was developer by Jaden Lund using ReactJS, and
+      TailwindCSS. Learn more about me <a>here</a>
+    </p>
+  );
+}
