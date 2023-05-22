63829b98f70bd05483b98a708cfec9550664a40f
JadenLund <lundjaden@gmail.com>
2023-05-22 13:14:08 -0500

    changing testmap in main

--- src/components/testmap.jsx
+++ src/components/testmap.jsx
@@ -3,6 +3,10 @@ import Catmix2 from "../assets/projectImages/Catmix2.png";
 import PUPPUR1 from "../assets/projectImages/PUPPUR1.png";
 import PUPPUR2 from "../assets/projectImages/PUPPUR2.png";
 import PUPPUR3 from "../assets/projectImages/PUPPUR3.png";
+import Artbyspecks1 from "../assets/projectImages/Artbyspecks1.png";
+import Artbyspecks2 from "../assets/projectImages/Artbyspecks2.png";
+import Artbyspecks3 from "../assets/projectImages/Artbyspecks3.png";
+import Artbyspecks4 from "../assets/projectImages/Artbyspecks4.png";
 
 //i need a container named projects, with each project inside that then has multiple images that will be mapped out
 export const projects = [
@@ -19,5 +23,12 @@ export const projects = [
     description:
       "Puppur is an innovative application built with Ruby on Rails and React, designed to provide users with an immersive educational experience about various dog breeds. By logging in, users gain access to a wealth of information, captivating visuals, and interactive features. Puppur allows users to browse through an extensive collection of dog breeds, enabling them to favorite their preferred ones for quick reference. Additionally, users can engage with the community by leaving comments and sharing their thoughts on different dog breeds, fostering a lively and informative platform for dog enthusiasts. With Puppur, learning about dog breeds becomes an enjoyable and personalized journey, making it an indispensable tool for dog lovers of all kinds.",
     extra: "Ruby on Rails | ReactJS | Heroku | SemanticUI",
+  },
+  {
+    title: "ArtBySpecks",
+    images: [Artbyspecks1, Artbyspecks2, Artbyspecks3, Artbyspecks4],
+    description:
+      "Artbyspecks is a work-in-progress application that showcases my personal art. It aims to provide a platform where I can share my unique creations and show others my artistic process. Get ready to immerse yourself in a world of beauty and creativity as I share my art with you. Stay tuned for an unforgettable experience that celebrates the power of self-expression.",
+    extra: "ReactJS | CSS",
   },
 ];
