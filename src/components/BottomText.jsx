f47eff1b4fea3783fdc2768d96d8cbc41a0ce507
JadenLund <lundjaden@gmail.com>
2023-06-04 22:48:26 -0500

    adding in

--- src/components/BottomText.jsx
+++ src/components/BottomText.jsx
@@ -2,7 +2,8 @@ export default function Bottomtext() {
   return (
     <p>
       This application was developer by Jaden Lund using ReactJS, and
-      TailwindCSS. Learn more about me <a>here</a>
+      TailwindCSS. Learn more about me{" "}
+      <a href="https://www.linkedin.com/in/jaden-lund/">here</a>
     </p>
   );
 }
