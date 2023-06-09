b4ed7f927e139737d5b6375704abbc93bfa89774
JadenLund <lundjaden@gmail.com>
2023-06-08 23:47:12 -0500

    adding in tcss

--- src/components/BottomText.jsx
+++ src/components/BottomText.jsx
@@ -1,6 +1,6 @@
 export default function Bottomtext() {
   return (
-    <p>
+    <p className="align-center">
       This application was developer by Jaden Lund using ReactJS, and
       TailwindCSS. Learn more about me{" "}
       <a href="https://www.linkedin.com/in/jaden-lund/">here</a>
